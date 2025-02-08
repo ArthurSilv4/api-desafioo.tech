0c48b60b1aed08556163cad453885a87aaf71d09
Arthur De Souza Silva <arthurdesouza.silv@gmail.com>
2025-02-08 14:14:01 -0300

    criado endpoint de delete

--- api-desafioo.tech/Controllers/ChallengeController.cs
+++ api-desafioo.tech/Controllers/ChallengeController.cs
@@ -111,5 +111,35 @@ namespace api_desafioo.tech.Controllers
 
             return Ok(challengeDto);
         }
+
+        [HttpDelete("DeleteChallenge")]
+        [Authorize]
+        public async Task<IActionResult> DeleteChallenge(Guid challengeId, CancellationToken ct)
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+            if (userIdClaim == null)
+            {
+                return Unauthorized();
+            }
+            if (!Guid.TryParse(userIdClaim.Value, out var userId))
+            {
+                return BadRequest("ID de usuário inválido.");
+            }
+            var challenge = await _context.Challenges.FindAsync(new object[] { challengeId }, ct);
+            if (challenge == null)
+            {
+                return NotFound("Desafio não encontrado.");
+            }
+            if (challenge.AuthorId != userId)
+            {
+                return Unauthorized();
+            }
+            _context.Challenges.Remove(challenge);
+            await _context.SaveChangesAsync(ct);
+
+            var challengeDto = new ChallengeDto(challenge.Id, challenge.Title, challenge.Description, challenge.Dificulty, challenge.Category, challenge.AuthorName, challenge.Links);
+
+            return Ok(new { message = "Desafio excluido", challengeDto });
+        }
     }
 }
