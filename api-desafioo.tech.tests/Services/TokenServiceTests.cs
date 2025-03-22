4ef5f205f81f8cd6c06637a6dbedcb2b861a9570
Arthur de Souza Silva <arthurdesouza.silv@gmail.com>
2025-03-22 17:31:57 -0300

    Adicionar testes para TokenService e ClaimsIdentity

--- api-desafioo.tech.tests/Services/TokenServiceTests.cs
+++ api-desafioo.tech.tests/Services/TokenServiceTests.cs
@@ -5,6 +5,7 @@ using Bogus;
 using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using Xunit.Abstractions;
 
 namespace api_desafioo.tech.tests.Services
@@ -51,6 +52,35 @@ namespace api_desafioo.tech.tests.Services
             Assert.False(string.IsNullOrEmpty(refreshToken), "RefreshToken should not be null or empty");
 
             _testOutputHelper.WriteLine($"RefreshToken: {refreshToken}");
+        }
+
+        [Fact]
+        public void TokenService_GetPrincipalFromExpiredToken_ShouldReturnValidPrincipal()
+        {
+            var user = new User(_faker.Person.UserName, _faker.Person.Email, _faker.Internet.Password());
+            var tokenService = new TokenService();
+            var expectedToken = tokenService.GenerateToken(user);
+
+            var principal = tokenService.GetPrincipalFromExpiredToken(expectedToken);
+
+            principal.Should().NotBeNull();
+
+            _testOutputHelper.WriteLine($"Principal: {principal}");
+        }
+
+        [Fact]
+        public void TokenService_ClaimsIdentity_ShouldGenerateClaims()
+        {
+            var user = new User(_faker.Person.UserName, _faker.Person.Email, _faker.Internet.Password());
+            var claimsIdentity = TokenService.GenerateClaims(user);
+
+            claimsIdentity.Should().NotBeNull();
+            claimsIdentity.Claims.Should().NotBeEmpty();
+            claimsIdentity.Claims.Should().Contain(c => c.Type == ClaimTypes.NameIdentifier && c.Value == user.Id.ToString());
+            claimsIdentity.Claims.Should().Contain(c => c.Type == ClaimTypes.Name && c.Value == user.Name);
+            claimsIdentity.Claims.Should().Contain(c => c.Type == ClaimTypes.Email && c.Value == user.Email);
+
+            _testOutputHelper.WriteLine($"ClaimsIdentity.Claims: {string.Join(", ", claimsIdentity.Claims)}");
         }
     }
 }
