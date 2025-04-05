9c7786275bc0a8de7d914d819afd5cfc196e9287
Arthur de Souza Silva <arthurdesouza.silv@gmail.com>
2025-04-05 18:08:58 -0300

    Criado endpoint de ping

---
+++ api-desafioo.tech/Controllers/PingController.cs
@@ -0,0 +1,17 @@
+﻿using api_desafioo.tech.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_desafioo.tech.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            var dateTime = DateTimeHelper.GetBrasiliaTime();
+            return Ok(dateTime);
+        }
+    }
+}
